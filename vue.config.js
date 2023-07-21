5bacc0dd9ba6734203233c9d6ccbe9a16e7ccf96
Alycia Canavan <acanavan@allbridge.com>
2023-07-21 00:48:03 -0700

    Revert config file for localhost to still work

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/10x-travel-client/' : '/',
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   transpileDependencies: true
-};
+})
